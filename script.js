3aa218de2a7b4af1a9887d0427731cc74b8e66d7
Knuths1 <h21emikn@du.se>
2022-11-07 16:12:51 +0100

    Fixat funktioner till sökningen via JS

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
 'use strict';
 /* Vad ska vi göra här? */
 
+const bookList = [
+    {
+        id: 1,
+        author: 'Charles Dickens',
+        title: 'Oliver Twist'
+    },
+    {
+        id:2,
+        author: 'William Shakespear',
+        title: 'Hamlet'
+    }
+];
+const searchInput = null;
+
+
+function handleKeyPress(input) {
+    /*Ta emot/läsa av värdet i inputfältet 
+    Skicka detta värde till searchBooks
+    searchBooks returnerar en filtrerad lista
+    Listan skickas till renderBookList
+    */
+    searchBooks(input);
+}
+
+function searchBooks(searchTerm){
+    /* Loopa igenom bookList
+    för varje varv i loopen, ta den aktuella boken
+    jämför titeln med söktermen
+    in Söktermen finn någonstans i titeln, lägg till boken i ny lista (filteredList)
+    */
+   let filteredList = []
+   for(let i = 0; i < bookList.length; i++){
+        const title = bookList[i].title.toLowerCase();
+            if (title.indexOf(searchTerm.toLowerCase()) >= 0){
+                filteredList.push(bookList[0]);
+            }
+    }
+    renderBookList(filteredList);
+}
+
+
+
+function renderBookList(list){
+    /*Element i HTML-listan visas/döljs beroende på listans innehåll */
+    console.log(list);
+}
+
+
+
+handleKeyPress('e')
