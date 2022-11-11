e44753af3c0de695366226030aa58a6d844541c7
Knuths1 <h21emikn@du.se>
2022-11-11 09:27:39 +0100

    Genererat HTML lista

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const bookList = [
 ];
 //const searchInput = document.children[0].children[1].children[1].children[1];
 const searchField = document.getElementById('searchField');
-console.log(searchField);
+//console.log(searchField);
 
 // keydown, keyup
 searchField.addEventListener('keyup', handleKeyPress);
@@ -26,8 +26,7 @@ function handleKeyPress(input) {
     searchBooks returnerar en filtrerad lista
     Listan skickas till renderBookList
     */
-   searchBooks(input.target.value)
-  searchBooks(input);
+  searchBooks(input.target.value)
 }
 
 function searchBooks(searchTerm) {
@@ -46,9 +45,26 @@ function searchBooks(searchTerm) {
   renderBookList(filteredList);
 }
 
-function renderBookList(list) {
+function renderBookList(bookList) {
   /*Element i HTML-listan visas/döljs beroende på listans innehåll */
-  console.log(list);
+  let html = `<ul class="book-list rounded-md border-2 border-blue-400 bg-white w-full mx-auto my-2">`
+  for(let i = 0; i < bookList.length; i++){
+    html +=`<li
+              class="book-list__item mb-2 mx-2 last:mb-0 p-3 text-indigo-900 last:border-b-0 border-b border-indigo-700 cursor-pointer"> 
+              ${bookList[i].author} - ${bookList[i].title}
+              </li>`;
+  }              
+  html +=`</ul>`
+  const existingElement = document.querySelector(".book-list");
+  const root = document.getElementById('root');
+  if (existingElement){
+    root.removeChild(existingElement);
+    
+  }
+  root.insertAdjacentHTML("beforeend", html)
+
+
+  //console.log(list);
 }
 
 
