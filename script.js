b4c9d2144f4bdf4e93af3587f25e4b0f726304de
Knuths1 <h21emikn@du.se>
2022-11-21 16:12:36 +0100

    api-kommunikation

--- script.js
+++ script.js
@@ -1,39 +1,20 @@
 "use strict";
 
-const bookList = [
-  {
-    id: 1,
-    author: "Charles Dickens",
-    title: "Oliver Twist",
-  },
-  {
-    id: 2,
-    author: "William Shakespear",
-    title: "Hamlet",
-  },
-];
+let bookList = [];
 
-//const searchField = document.getElementById('searchField');
+window.addEventListener("load", ()=> {
+  getAll().then(apiBooks => (bookList = apiBooks))
+})
 
-//searchField.addEventListener('keyup', (e) => searchBooks(e.target.value));
-searchField.addEventListener('keyup', (e) => 
-renderBookList(
-  bookList.filter(({ title, author }) => {
-    const searchTerm = e.target.value.toLowerCase();
-    return title.toLowerCase().indexOf(searchTerm) >= 0 || author.toLowerCase().indexOf(searchTerm) >= 0;
-  })
-)
-);
+searchField.addEventListener('keyup', (e) => searchBooks(e.target.value));
 
-
-// function searchBooks(searchTerm) {
-//   renderBookList(
-//   bookList.filter(({ title, author }) =>  
-//   (title.toLowerCase().indexOf(searchTerm.toLowerCase()) >= 0 ||
-//   author.toLowerCase().indexOf(searchTerm.toLowerCase()) >= 0))
-//   );
-// }
-
+async function searchBooks(searchTerm) {
+  renderBookList(
+  bookList.filter(({ title, author }) =>  
+  (title.toLowerCase().indexOf(searchTerm.toLowerCase()) >= 0 ||
+  author.toLowerCase().indexOf(searchTerm.toLowerCase()) >= 0))
+  );
+}
 
 function renderBookList(bookList) {
   /*Element i HTML-listan visas/döljs beroende på listans innehåll */
@@ -42,10 +23,13 @@ function renderBookList(bookList) {
   if (existingElement){
     root.removeChild(existingElement);   
   }
-  if(bookList.length > 0){
+  if(bookList.length > 0 && searchField.value){
     root.insertAdjacentHTML("beforeend", BookList(bookList));
   }
 }
+
+
+getAll().then(bookList => console.log(bookList));
 
 
 
