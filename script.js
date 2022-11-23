8b04b296673cda5d305ec951a0cda8372cd6e097
Knuths1 <h21emikn@du.se>
2022-11-23 09:07:18 +0100

    sistsa

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 let bookList = [];
 
-window.addEventListener("load", ()=> {
+window.addEventListener("load", () => {
   getAll().then(apiBooks => (bookList = apiBooks))
 })
 
@@ -10,9 +10,9 @@ searchField.addEventListener('keyup', (e) => searchBooks(e.target.value));
 
 async function searchBooks(searchTerm) {
   renderBookList(
-  bookList.filter(({ title, author }) =>  
-  (title.toLowerCase().indexOf(searchTerm.toLowerCase()) >= 0 ||
-  author.toLowerCase().indexOf(searchTerm.toLowerCase()) >= 0))
+    bookList.filter(({ title, author }) =>
+    (title.toLowerCase().indexOf(searchTerm.toLowerCase()) >= 0 ||
+      author.toLowerCase().indexOf(searchTerm.toLowerCase()) >= 0))
   );
 }
 
@@ -20,16 +20,14 @@ function renderBookList(bookList) {
   /*Element i HTML-listan visas/döljs beroende på listans innehåll */
   const existingElement = document.querySelector(".book-list");
   const root = document.getElementById('root');
-  if (existingElement){
-    root.removeChild(existingElement);   
+  if (existingElement) {
+    root.removeChild(existingElement);
   }
-  if(bookList.length > 0 && searchField.value){
+  if (bookList.length > 0 && searchField.value) {
     root.insertAdjacentHTML("beforeend", BookList(bookList));
   }
 }
 
-
-getAll().then(bookList => console.log(bookList));
 
 
 
