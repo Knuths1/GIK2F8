23202d40d2517d042cc97808f2d9ddd4c978ef4c
Knuths1 <h21emikn@du.se>
2022-12-05 17:37:28 +0100

    Delete

--- 02-todo/client/Api.js
+++ 02-todo/client/Api.js
@@ -5,6 +5,7 @@ class Api {
     this.url = url;
   }
 
+  /*Create = POST */
   create(data) {
     const JSONData = JSON.stringify(data);
     console.log(`Skickar ${JSONData} to this ${this.url}`);
@@ -16,15 +17,29 @@ class Api {
         "content-type": "application/json",
       },
     });
-    console.log(request);
-    console.log("Hallå");
+
     return fetch(request)
       .then((result) => result.json())
       .then((data) => data)
       .catch((err) => console.log(err));
   }
 
-  getAll() {}
+  /*READ = GET */
+  getAll() {
+    return fetch(this.url)
+      .then((result) => result.json())
+      .then((data) => data)
+      .catch((err) => console.log(err));
+  }
 
-  remove() {}
+  /*remove = DELETE */
+  remove(id) {
+    console.log(`Removing task with id ${id}`);
+
+    return fetch(`${this.url}/${id}`, {
+      method: "DELETE",
+    })
+      .then((result) => result)
+      .catch((err) => console.log(err));
+  }
 }
--- 02-todo/client/script.js
+++ 02-todo/client/script.js
@@ -7,6 +7,7 @@ todoForm.dueDate.addEventListener("blur", (e) => validateField(e.target));
 
 todoForm.addEventListener("submit", onSubmit);
 
+const todoListElement = document.getElementById("todoList");
 let titleValid = true;
 let descriptionValid = true;
 let dueDateValid = true;
@@ -27,6 +28,8 @@ function validateField(field) {
         titleValid = false;
         validationMessage =
           "Fältet 'Titel' får inte innehålla mer än 100 tecken";
+      } else {
+        titleValid = true;
       }
       break;
     }
@@ -35,6 +38,8 @@ function validateField(field) {
         descriptionValid = false;
         validationMessage =
           "Fältet 'Beskrivning' får inte innehålla mer än 500 tecken";
+      } else {
+        descriptionValid = true;
       }
       break;
     }
@@ -42,6 +47,8 @@ function validateField(field) {
       if (value.length === 0) {
         dueDateValid = false;
         validationMessage = "Fältet 'Slutfört senast' är obligatorisk";
+      } else {
+        dueDateValid = true;
       }
       break;
     }
@@ -57,23 +64,55 @@ function onSubmit(e) {
     console.log("Submit");
     saveTask();
   }
-
-  function saveTask() {
-    const task = {
-      title: todoForm.title.value,
-      description: todoForm.description.value,
-      dueDate: todoForm.dueDate.value,
-      completed: false,
-    };
-
-    api.create(task).then((task) => {
-      if (task) {
-        render();
-      }
-    });
-  }
 }
 
-function render() {
-  console.log("rendering");
+function saveTask() {
+  const task = {
+    title: todoForm.title.value,
+    description: todoForm.description.value,
+    dueDate: todoForm.dueDate.value,
+    completed: false,
+  };
+
+  api.create(task).then((task) => {
+    if (task) {
+      renderList();
+    }
+  });
 }
+
+function renderList() {
+  api.getAll().then((tasks) => {
+    todoListElement.innerHTML = "";
+    if (tasks && tasks.length > 0) {
+      tasks.forEach((task) => {
+        todoListElement.insertAdjacentHTML("beforeend", renderTask(task));
+      });
+    }
+  });
+}
+
+function renderTask({ id, title, description, dueDate, completed }) {
+  let html = `
+    <li class="select-none mt-2 py-2 border-b border-amber-300">
+        <div class="flex items-center">
+            <h3 class="mb-3 flex-1 text-xl font-bold text-pink-800 uppercase">${title}</h3>
+            <div>
+                <span>${dueDate}</span>
+                <button onclick="deleteTask(${id})" class="inline-block bg-amber-500 text-xs text-amber-900 border-white px-3 py-1 rounded-md ml-2">Ta bort</button>
+            </div>
+        </div>
+    `;
+  description &&
+    (html += `<p class="ml-8 mt-2 text-xs italic">${description}</p>`);
+  html += `</li>`;
+  return html;
+}
+
+function deleteTask(id) {
+  api.remove(id).then((result) => {
+    renderList();
+  });
+}
+
+renderList();
